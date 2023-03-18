b354e83e2a4b58beae926ef4e0d41a4aed8d9b26
joelmalacas <54784217+joelmalacas@users.noreply.github.com>
2023-03-18 04:51:12 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,6 +5,7 @@ python -m pip install --upgrade pip
 pip install folium
 pip install phonenumbers
 pip install geocoder
+pip install figlet
 pip install opencage
 echo Bibliotecas Instaladas Com Sucesso
 exit
